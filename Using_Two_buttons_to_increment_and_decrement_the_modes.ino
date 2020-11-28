2a3c0ecb7c8a70cc55a6d49939684591ea4f14aa
AdityapratapSingh02 <75161832+AdityapratapSingh02@users.noreply.github.com>
2020-11-28 15:52:01 +0530

    Add files via upload
    "Using_Two_buttons_to_increment_and_decrement_the_modes" this code uses 2 buttons to increment and decrement through the modes, the user can add as many modes as needed, just proper calibration is required as per the application.

---
+++ Using_Two_buttons_to_increment_and_decrement_the_modes.ino
@@ -0,0 +1,95 @@
+const PROGMEM int Mode1Select = 2;                   //Button to increment the mode
+const PROGMEM int Mode2Select = 3;                   //Button to decrement the mode
+
+
+int ChPushCounter = 0;   // counter for the number of button presses
+int ChIncState = 0;         // current state of the button
+int ChDecState = 0;         // current state of the button
+int lastChIncState = 1;     // previous state of the button
+int lastChDecState = 1;     // previous state of the button
+
+void setup() {
+  pinMode(Mode1Select, INPUT_PULLUP);
+  pinMode(Mode2Select, INPUT_PULLUP);
+  Serial.begin(9600);
+}
+
+void loop() {
+  ChannelSelectFunction();
+}
+
+
+
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^Channel Select Function
+void ChannelSelectFunction() {
+  ChIncState = digitalRead(Mode1Select);
+
+  // compare the buttonState to its previous state
+  if (ChIncState != lastChIncState) {
+    // if the state has changed, increment the counter
+    if (ChIncState == LOW && ChPushCounter <= 3) {
+      // if the current state is HIGH then the button went from off to on:
+      ChPushCounter++;
+    }
+    else if (ChIncState == LOW && ChPushCounter == 4) {
+      // if the current state is HIGH then the button went from off to on:
+      ChPushCounter = 1;
+    }
+    // save the current state as the last state, for next time through the loop
+    lastChIncState = ChIncState;
+    if (ChPushCounter == 1 && ChIncState == LOW) {
+      Serial.println("Mode increment 1");
+      delay(100);
+    }
+    else if (ChPushCounter == 2 && ChIncState == LOW) {
+      Serial.println("Mode increment 2");
+      delay(100);
+    }
+
+    else if (ChPushCounter == 3 && ChIncState == LOW) {
+      Serial.println("Mode increment 3");
+      delay(100);
+    }
+    else if (ChPushCounter == 4 && ChIncState == LOW) {
+      Serial.println("Mode increment 4");
+      delay(100);
+    }
+  }
+
+  // For decreasing the channel
+  ChDecState = digitalRead(Mode2Select);
+
+  // compare the buttonState to its previous state
+  if (ChDecState != lastChDecState) {
+    // if the state has changed, increment the counter
+    if (ChDecState == LOW && ChPushCounter >= 2) {
+      // if the current state is HIGH then the button went from off to on:
+      ChPushCounter--;
+    }
+    else if (ChDecState == LOW && ChPushCounter == 1) {
+      // if the current state is HIGH then the button went from off to on:
+      ChPushCounter = 4;
+    }
+    // save the current state as the last state, for next time through the loop
+    lastChDecState = ChDecState;
+    if (ChPushCounter == 1 && ChDecState == LOW) {
+      Serial.println("Mode decrement 1");
+      delay(100);
+    }
+    else if (ChPushCounter == 2 && ChDecState == LOW) {
+      Serial.println("Mode decrement 2");
+      delay(100);
+    }
+
+    else if (ChPushCounter == 3 && ChDecState == LOW) {
+      Serial.println("Mode decrement 3");
+      delay(100);
+    }
+    else if (ChPushCounter == 4 && ChDecState == LOW) {
+      Serial.println("Mode decrement 4");
+      delay(100);
+    }
+  }
+
+
+}
